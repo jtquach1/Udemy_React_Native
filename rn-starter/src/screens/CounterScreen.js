8c3ae4ec203f00b41015620b6566234a2a231122
Joyce Quach <33106214+jtquach1@users.noreply.github.com>
2020-05-29 15:25:46 -0400

    Update CounterScreen.js
    To use useReducer instead of useState hook

--- rn-starter/src/screens/CounterScreen.js
+++ rn-starter/src/screens/CounterScreen.js
@@ -1,25 +1,41 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
-const CounterScreen = () => {
-    const [counter, setCounter] = useState(0);
+const reducer = (state, action) => {
+    switch (action.type) {
+        // probably don't need to switch on type, but good to do
+        // use ...state for convention
+        case 'increase':
+            return {...state, counter: state.counter + action.payload};
+        case 'decrease':
+            return {...state, counter: state.counter + action.payload};
+        default:
+            return state
+    }
+};
 
+const CounterScreen = () => {
+    // init state to { counter: 0 }
+    // dispatch calls reducer with the state and supplied action
+    // dispatch => useReducer => reducer 
+    const [state, dispatch] = useReducer(reducer, {counter: 0});
+    // get counter from the state
+    const { counter } = state;
+
+    // can specify payload here
     return (
         <View>
             <Button title="Increase" onPress={() => {
-                setCounter(counter + 1);
-                console.log(counter);
+                dispatch({ type: 'increase', payload: +1 });
             }}/>
             <Button title="Decrease" onPress={() => {
-                setCounter(counter - 1);
+                dispatch({ type: 'decrease', payload: -1 });
             }}/>
             <Text>Current Count: {counter}</Text>
         </View>
     );
 };
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({ });
 
 export default CounterScreen;
