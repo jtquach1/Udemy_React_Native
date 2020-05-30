bf625af4ca879d404509444b0f58e05d384050d7
Joyce Quach <33106214+jtquach1@users.noreply.github.com>
2020-05-30 19:13:06 -0400

    Testing position property

--- rn-starter/src/screens/BoxScreen.js
+++ rn-starter/src/screens/BoxScreen.js
@@ -16,21 +16,21 @@ const styles = StyleSheet.create({
         borderWidth: 3,
         borderColor: 'black',
         height: 200,
+        alignItems: 'flex-end',
     },
     textOneStyle: {
         borderWidth: 3,
         borderColor: 'red',
-        flex: 4,
     },
     textTwoStyle: {
         borderWidth: 3,
         borderColor: 'red',
-        flex: 4,
+        position: 'absolute',
+        fontSize: 18,
     },
     textThreeStyle: {
         borderWidth: 3,
         borderColor: 'red',
-        flex: 2,
     },
 });
 
