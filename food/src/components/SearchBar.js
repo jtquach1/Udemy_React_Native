f794fcd1c73d94403e66cc5e20314d5214213c1b
Joyce Quach <33106214+jtquach1@users.noreply.github.com>
2020-05-31 13:31:17 -0400

    Style SearchBar and include in SearchScreen

--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -3,12 +3,21 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const SearchBar = () => {
     return (
-        <View>
-            <Text>Search Screen</Text>
+        <View style={styles.background}>
+            <Text>Search Bar</Text>
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    // for the text input and icon to show inside
+    background: {
+        // can specify color: 'white', 'rgb(255,255,255)', or '#fff'
+        backgroundColor: '#F0EEEE',
+        height: 50,
+        borderRadius: 5,
+        marginHorizontal: 15,
+    }
+});
 
 export default SearchBar;
