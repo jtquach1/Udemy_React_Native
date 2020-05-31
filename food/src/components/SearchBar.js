5afa992569d781e9f7fea9a09f3f747d9c16b293
Joyce Quach <33106214+jtquach1@users.noreply.github.com>
2020-05-31 13:53:12 -0400

    Update SearchBar.js

--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, TextInput, StyleSheet } from 'react-native';
 // import the icon library
 import { Feather } from '@expo/vector-icons';
 
 const SearchBar = () => {
     // pass the name as a prop
     return (
-        <View style={styles.background}>
-            <Feather name="search" size={30} color="black" />
-            <Text>Search Bar</Text>
+        <View style={styles.backgroundStyle}>
+            <Feather name="search" size={30} />
+            <TextInput style={styles.inputStyle} placeholder="Search" />
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    background: {
+    backgroundStyle: {
         backgroundColor: '#F0EEEE',
         height: 50,
         borderRadius: 5,
         marginHorizontal: 15,
-    }
+        flexDirection: 'row',
+    },
+    inputStyle: {
+        borderColor: 'black',
+        borderWidth: 1,
+        flex: 1,
+    },
 });
 
 export default SearchBar;
