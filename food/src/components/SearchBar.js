6e466d885f2a49e8fa66483a77dec86349989d19
Joyce Quach <33106214+jtquach1@users.noreply.github.com>
2020-05-31 13:43:48 -0400

    Update SearchBar.js
    Add the search icon with some styling

--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+// import the icon library
+import { Feather } from '@expo/vector-icons';
 
 const SearchBar = () => {
+    // pass the name as a prop
     return (
         <View style={styles.background}>
+            <Feather name="search" size={30} color="black" />
             <Text>Search Bar</Text>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    // for the text input and icon to show inside
     background: {
-        // can specify color: 'white', 'rgb(255,255,255)', or '#fff'
         backgroundColor: '#F0EEEE',
         height: 50,
         borderRadius: 5,
