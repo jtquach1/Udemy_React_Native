d4a19290826c9aad777d842f7937ace083f52eb7
Joyce Quach <33106214+jtquach1@users.noreply.github.com>
2020-06-10 17:51:19 -0400

    Navigating from a Child Component

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { Text, StyleSheet, ScrollView } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
 import ResultsList from '../components/ResultsList';
 
-const SearchScreen = (props) => {
-    console.log(props);
-    
+const SearchScreen = ({ navigation }) => {
+
     const [term, setTerm] = useState('');
     const [searchApi, results, errorMessage] = useResults();
     
@@ -27,9 +26,21 @@ const SearchScreen = (props) => {
             {errorMessage ? <Text>{errorMessage}</Text> : null}
 
             <ScrollView>
-                <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
-                <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
-                <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender!" />
+                <ResultsList 
+                    results={filterResultsByPrice('$')} 
+                    title="Cost Effective" 
+                    navigation={navigation}
+                />
+                <ResultsList 
+                    results={filterResultsByPrice('$$')} 
+                    title="Bit Pricier" 
+                    navigation={navigation}
+                />
+                <ResultsList 
+                    results={filterResultsByPrice('$$$')} 
+                    title="Big Spender!" 
+                    navigation={navigation}
+                />
             </ScrollView>
         </>
     );
